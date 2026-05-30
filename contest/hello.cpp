ec2aa065dd6dff3398378f081d24dd389dda698c
Atul singh <atusingh1618@gmail.com>
2026-05-30 13:22:55 +0530

    added solution for 3 question in contest 1

---
+++ contest/hello.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    long long n;
+    cin >> n;
+    for (long long i = 0; i < n; i++)
+    {
+        cout<< "Hello Codeforces " << i+1 << "\n";
+    }
+    
+}
