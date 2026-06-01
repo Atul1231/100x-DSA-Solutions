aa23c88356353cb18e4198d694836de94ac05ef9
Atul singh <atusingh1618@gmail.com>
2026-06-01 15:25:29 +0530

    added solution to array module

---
+++ Arrays/minArray.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    int pos=1;
+    int min=arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i]<min){
+            pos=i+1;
+            min=arr[i];
+        }
+    }
+    cout<<min<<"\t"<<pos;
+    
+}
