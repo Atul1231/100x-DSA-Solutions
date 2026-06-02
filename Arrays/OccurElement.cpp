11cebd2decefe23550ad6524608818e7854b8790
Atul singh <atusingh1618@gmail.com>
2026-06-02 15:03:47 +0530

    added some more solutions to the array module

---
+++ Arrays/OccurElement.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,num;
+    cin>>n>>num;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    int count=0;
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i]==num){
+            count++;
+        }
+    }
+    cout<<count;
+}
