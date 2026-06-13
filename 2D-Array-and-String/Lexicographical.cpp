136aa6eb2aaae32a676fb8ddc7b358d8dbeac667
Atul singh <atusingh1618@gmail.com>
2026-06-13 22:13:26 +0530

    added solution to 2d array

---
+++ 2D-Array-and-String/Lexicographical.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string s1,s2;
+    cin>>s1>>s2;
+    if(s1<s2){
+        cout<<"A";
+    }
+    else if(s1>s2)
+    {
+        cout<<"B";
+    }
+    else
+    {
+        cout<<"Equal";
+    }
+    
+        
+}
